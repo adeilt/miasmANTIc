66cabb19a8196f0ff1e4d3c460a6eec0778c22e2
adeilt <56762639+adeilt@users.noreply.github.com>
2023-02-26 22:43:27 -0800

    Initial commit

---
+++ src/content.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+import "./content.css";
+
+const container_id = 'sidecar_container';
+
+// Create the container and add it to the body
+const sidecar_container = document.createElement('div');
+sidecar_container.id = container_id;
+document.body.appendChild(sidecar_container);
+
+// Create the root and render the Sidecar into it
+const root = createRoot(sidecar_container);
+root.render(<Sidecar />);
+
+// The only React component in this whole mess.  :)
+function Sidecar () {
+  return <div id="sidecar">I'm a sidecar!  Vroom!</div>;
+}
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'cheap-module-source-map',
+  entry: {
+    content: './src/content.jsx',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        use: ['babel-loader'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: 'LICENSE' },
+        { from: 'src/manifest.json' },
+        { from: 'src/icons/', to: 'icons/'},
+      ],
+    }),
+  ],
+};
