fcc85f8fa74c5798a7deaa09360186fab4f754d9
Wil adeilt Langford <adeilt.wsl@gmail.com>
2023-02-27 10:00:28 -0800

    Move Sidecar component into its own file.

---
+++ src/components/Sidecar.jsx
@@ -0,0 +1,9 @@
+import './Sidecar.css';
+
+export default function Sidecar() {
+  return (
+    <div id="sidecar">
+      <h1>miasmANTIc</h1>
+    </div>
+  );
+}
--- src/content.jsx
+++ src/content.jsx
@@ -1,19 +1,16 @@
-import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 import './content.css';
 
+import Sidecar from './components/Sidecar.jsx';
+
 const container_id = 'sidecar_container';
 
 // Create the container and add it to the body
-const sidecar_container = <div id={container_id}></div>;
+const sidecar_container = document.createElement('div');
+sidecar_container.id = container_id;
 document.body.appendChild(sidecar_container);
 
 // Create the root and render the Sidecar into it
 const root = createRoot(sidecar_container);
 root.render(<Sidecar />);
-
-// The only React component in this whole mess.  :)
-function Sidecar() {
-  return <div id="sidecar">I'm a sidecar! Vroom!</div>;
-}
